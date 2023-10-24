994c2c9d56e0a1b934d38ff71fe30394b775569a
brianochieng23 <ochiengbr23@gmail.com>
2023-10-24 14:58:45 +0300

    Barcode creation

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ let container = document.querySelector('.container');
 let generateBarcode = () => {
         container.classList.add('active');
         setTimeout(() => {
-                JsBarcode("#barcode", userInput.value, {
-                        background: "white",
-                        lineColor: "black",
-                });
-        }, 200)
-}
+            JsBarcode("#barcode", userInput.value, {
+                background: "white",
+                lineColor: "black",
+            });
+    
+            // Show the download button after generating the barcode
+            document.getElementById('downloadBtn').style.display = 'inline-block';
+        }, 200);
+    }
 
 generateBtn.addEventListener('click', () => {
         if (userInput.value != '') {
                 generateBarcode();
         }
 })
+    
+let downloadBarcode = () => {
+        let svgString = new XMLSerializer().serializeToString(barcode);
+        let blob = new Blob([svgString], { type: 'image/svg+xml;charset=utf-8' });
+        let url = URL.createObjectURL(blob);
+        let link = document.createElement('a');
+        link.href = url;
+        link.download = 'barcode.svg';
+        link.click();
+    
+        URL.revokeObjectURL(url);
+    }
+    
+    
